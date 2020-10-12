3ef053d076f9892f7436c4216b9dcd7d7c2e3eb4
Rafo <72395764+syltare@users.noreply.github.com>
2020-10-11 22:22:41 -0300

    Update matrixOptions.java

--- java/basicArrayMatrix/matrixOptions.java
+++ java/basicArrayMatrix/matrixOptions.java
@@ -1,3 +1,19 @@
+/*
+*
+* STILL TODO!
+*
+* Faça um programa que leia duas matrizes 2 x 2 com valores reais. Ofereça ao usuário um
+* menu de opções:
+* (1) somar as duas matrizes
+* (2) subtrair a primeira matriz da segunda
+* (3) adicionar uma constante as duas matrizes
+* (4) imprimir as matrizes
+* Nas duas primeiras opções uma terceira matriz 2 x 2 deve ser criada. Na terceira opção o valor
+* da constante deve ser lido e o resultado da adição da constante deve ser armazenado na
+* própria matriz.
+*
+*/
+
 import java.util.Scanner;
 
 public class matrixOptions{
