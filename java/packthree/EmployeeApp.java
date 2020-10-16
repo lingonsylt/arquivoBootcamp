f6c41b77fa2cbaa3b64e2f0a1f33435fa36cf4cc
Rafo <72395764+syltare@users.noreply.github.com>
2020-10-15 21:33:58 -0300

    Update EmployeeApp.java

--- java/packthree/EmployeeApp.java
+++ java/packthree/EmployeeApp.java
@@ -1,7 +1,7 @@
 public class EmployeeApp{
 	public static void main (String[] args){
 		Employee rafael = new Employee(
-			1,33225566, 15, "Rafael", "Java dev", "Rua da Marola", 2500, 10.50
+			1,33225566, 15, "Rafael", "Java dev", "Rua da Marola, 420", 2500, 10.50
 		);
 
 		rafael.EmployeeStatus();
