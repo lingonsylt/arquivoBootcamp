610f92d28f7e10e91e8d2b05a91888972b810b91
lingonsylt <rafael.rodrigues@tuta.io>
2020-11-01 23:36:18 -0300

    Refact

--- java/arrayBasics/ArraysOptions.java
+++ java/arrayBasics/ArraysOptions.java
@@ -21,33 +21,37 @@ import java.util.Scanner;
 public class ArraysOptions{
     public static void main (String[] arg){
         int option;
-        float[][] matrixOne = new float[2][2];
-        float[][] matrixTwo = new float[2][2];
-        float[][] matrixSum = new float[2][2];
-        float[][] matrixSub = new float[2][2];
+        int[][] matrixOne = new int[2][2];
+        int[][] matrixTwo = new int[2][2];
+        int[][] matrixSum = new int[2][2];
+        int[][] matrixSub = new int[2][2];
         Scanner s = new Scanner(System.in);
 
         for(int i = 0; i < 2; i++){
             for(int j = 0; j < 2; j++){
-                System.out.printf("Atribua um número para a posição [%d][%d] ",
-                        "da matriz 1: ", i, j);
-                matrixOne[i][j] = s.nextFloat();
+                System.out.printf("Atribua um número para a posição %d,%d"+
+                                  "da matriz 1: ", i, j);
+                matrixOne[i][j] = s.nextInt();
             }
         }
         for(int i = 0; i < 2; i++){
             for(int j = 0; j < 2; j++){
-                System.out.printf("Atribua um número para a posição [%d][%d] ",
-                        "da matriz 2: ", i, j);
-                matrixOne[i][j] = s.nextFloat();
+                System.out.printf("Atribua um número para a posição %d,%d"+
+                                  "da matriz 2: ", i, j);
+                matrixOne[i][j] = s.nextInt();
             }
         }
 
-        matrixSum = matrixOne + matrixTwo;
-        matrixSub = matrixTwo - matrixOne;
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 2; j++){
+                matrixSum[i][j] = matrixOne[i][j] + matrixTwo[i][j];
+                matrixSub[i][j] = matrixTwo[i][j] - matrixOne[i][j];
+            }
+        }
 
-        System.out.printf("Escolha uma opção:\n(1) Somar matrizes\n(2) Subtra",
-                          "ir primeira matrix da segunda\n(3) Somar uma const",
-                          "ante às matrizes\n(4) Mostrar matrizes\n(5) Finali",
+        System.out.printf("Escolha uma opção:\n(1) Somar matrizes\n(2) Subtra"+
+                          "ir primeira matrix da segunda\n(3) Somar uma const"+
+                          "ante às matrizes\n(4) Mostrar matrizes\n(5) Finali"+
                           "zar programa\n");
         option = s.nextInt();
 
@@ -55,7 +59,7 @@ public class ArraysOptions{
             case 1:
                 for(int i = 0; i < 2; i++){
                     for(int j = 0; j < 2; j++){
-                        System.out.printf("%f, ", matrixSum[i][j]);
+                        System.out.printf("%d, ", matrixSum[i][j]);
                     }
                 }
                 break;
@@ -63,7 +67,7 @@ public class ArraysOptions{
             case 2:
                 for(int i = 0; i < 2; i++){
                     for(int j = 0; j < 2; j++){
-                        System.out.printf("%f, ", matrixSub[i][j]);
+                        System.out.printf("%d, ", matrixSub[i][j]);
                     }
                 }
                 break;
--- java/arrayBasics/evenOddArray.java
+++ java/arrayBasics/evenOddArray.java
@@ -1,51 +1,48 @@
-/*
+/**
  * Program reads 6 integers and then prints out: the even numbers and
- * the  sum of them. The odd numbers and the sum of them.
+ * their sum, the odd numbers and their sum.
+ *
+ * Author: Rafael Oliveira.
+ *
+ * Last update: November 1, 2020 21:56:14
  */
 
 import java.util.Scanner;
 
-public class evenOddArray{
-	public static void main (String args[])
-	{
-		int[] integers = new int[6];
-		int even = 0, odd = 0;
-		Scanner s = new Scanner(System.in);
+public class EvenOddArray{
+    public static void main (String[] args){
+        int
+            odd = 0,
+                even = 0;
+        int[] integers = new int[6];
+        Scanner s = new Scanner(System.in);
 
-		for(int i = 0; i < 6; i++)
-		{
-			System.out.print("Digite um número: ");
-			integers[i] = s.nextInt();
+        for(int i = 0; i < 6; i++){
+            System.out.print("Digite um número: ");
+            integers[i] = s.nextInt();
 
-			if(integers[i] % 2 == 0)
-			{
-				even+= integers[i];
-			}
-			else
-			{
-				odd++;
-			}
-		}
-		
-		System.out.print("Lista dos números pares:\n");
-		for(int i = 0; i < 6; i++)
-		{
-			if(integers[i] % 2 == 0)
-			{
-				System.out.printf("%d ", integers[i]);
-			}
-		}
-		System.out.printf("\nA soma dos números pares é: %d\n", even);
+            if(integers[i] % 2 == 0){
+                even+= integers[i];
+            }else{
+                odd+= integers[i];
+            }
+        }
 
-		System.out.print("Lista dos números ímpares:\n");
-		for(int i = 0; i < 6; i++)
-		{
-			if(integers[i] % 2 == 1)
-			{
-				System.out.printf("%d ", integers[i]);
-			}
-		}
-		System.out.printf("\nA quantidade de números ímpares é: %d", odd);
+        System.out.print("Lista dos números pares:\n");
+        for(int integer: integers){
+            if(integer % 2 == 0){
+                System.out.printf("%d ", integer);
+            }
+        }
+        System.out.printf("\nA soma dos números pares é: %d\n", even);
 
-	}
+        System.out.print("Lista dos números ímpares:\n");
+        for(int integer: integers){
+            if(integer % 2 == 1){
+                System.out.printf("%d ", integer);
+            }
+        }
+        System.out.printf("\nA quantidade de números ímpares é: %d", odd);
+
+    }
 }
