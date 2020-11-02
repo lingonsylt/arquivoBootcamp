e34302d708d77424189c1e7b54909b3367affc93
lingonsylt <rafael.rodrigues@tuta.io>
2020-11-02 00:53:22 -0300

    Refact

--- java/arrayBasics/ArraysOptions.java
+++ java/arrayBasics/ArraysOptions.java
@@ -17,6 +17,7 @@
  */
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class ArraysOptions{
     public static void main (String[] arg){
@@ -30,52 +31,71 @@ public class ArraysOptions{
         for(int i = 0; i < 2; i++){
             for(int j = 0; j < 2; j++){
                 System.out.printf("Atribua um número para a posição %d,%d"+
-                                  "da matriz 1: ", i, j);
-                matrixOne[i][j] = s.nextInt();
-            }
-        }
-        for(int i = 0; i < 2; i++){
-            for(int j = 0; j < 2; j++){
-                System.out.printf("Atribua um número para a posição %d,%d"+
-                                  "da matriz 2: ", i, j);
+                        "da matriz 1: ", i, j);
                 matrixOne[i][j] = s.nextInt();
             }
         }
 
         for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 2; j++){
+                System.out.printf("Atribua um número para a posição %d,%d"+
+                        "da matriz 2: ", i, j);
+                matrixTwo[i][j] = s.nextInt();
+            }
+
             for(int j = 0; j < 2; j++){
                 matrixSum[i][j] = matrixOne[i][j] + matrixTwo[i][j];
                 matrixSub[i][j] = matrixTwo[i][j] - matrixOne[i][j];
             }
         }
 
-        System.out.printf("Escolha uma opção:\n(1) Somar matrizes\n(2) Subtra"+
-                          "ir primeira matrix da segunda\n(3) Somar uma const"+
-                          "ante às matrizes\n(4) Mostrar matrizes\n(5) Finali"+
-                          "zar programa\n");
+        do{
+        System.out.println("\nEscolha uma opção:\n(1) Somar matrizes\n(2) Sub"+
+                           "trair primeira matrix da segunda\n(3) Somar uma c"+
+                           "onstante às matrizes\n(4) Mostrar matrizes\n(5) F"+
+                           "inalizar programa\n");
         option = s.nextInt();
 
-        switch(option){
-            case 1:
-                for(int i = 0; i < 2; i++){
-                    for(int j = 0; j < 2; j++){
-                        System.out.printf("%d, ", matrixSum[i][j]);
-                    }
-                }
-                break;
+            switch(option){
+                case 1:
+                    System.out.println(Arrays.deepToString(matrixSum));
+                    break;
 
-            case 2:
-                for(int i = 0; i < 2; i++){
-                    for(int j = 0; j < 2; j++){
-                        System.out.printf("%d, ", matrixSub[i][j]);
-                    }
-                }
-                break;
-                
-            case 3:
-                System.out.print("Digite valor da constante: ");
-                break;
-        }
+                case 2:
+                    System.out.println(Arrays.deepToString(matrixSub));
+                    break;
 
+                case 3:
+                    System.out.print("Digite valor da constante: ");
+                    int constant = s.nextInt();
+
+                    for(int i = 0; i < 2; i++){
+                        for(int j = 0; j < 2; j++){
+                            matrixOne[i][j]+= constant;
+                        }
+                    }
+                    System.out.println(Arrays.deepToString(matrixOne));
+
+                    for(int i = 0; i < 2; i++){
+                        for(int j = 0; j < 2; j++){
+                            matrixTwo[i][j]+= constant;
+                        }
+                    }
+                    System.out.println(Arrays.deepToString(matrixTwo));
+                    break;
+
+                case 4:
+                    System.out.println(Arrays.deepToString(matrixOne));
+                    System.out.println(Arrays.deepToString(matrixTwo));
+                    break;
+
+                case 5:
+                    option = -1;
+                    break;
+
+                default:
+                    System.out.println("Escolha uma opção válida");
+            }
+        }while(option != -1);
     }
 }
